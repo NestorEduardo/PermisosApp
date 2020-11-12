4e58bb2f8106a1649476b7625707fee3705b0940
Nestor De La Cruz <ndelacruz@evopointsolution.com>
2020-11-12 02:05:07 -0400

    Created base entity, PermissionType entity and the Data project

---
+++ PermisosApp/PermisosApp.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PermisosApp.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Modified { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+    }
+}
